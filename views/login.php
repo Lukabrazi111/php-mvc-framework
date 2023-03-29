987c62186a380ce29f1c37f0f82854c73b68cebf
Lukabrazi111 <lukabrazi@redberry.ge>
2023-03-29 22:04:08 +0400

    style: login page

--- views/login.php
+++ views/login.php
@@ -1,1 +1,13 @@
 <h1>Sign In</h1>
+
+<form action="" method="post">
+    <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control" name="email" id="password">
+    </div>
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control" name="password" id="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Sign In</button>
+</form>
